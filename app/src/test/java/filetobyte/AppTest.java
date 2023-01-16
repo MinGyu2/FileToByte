ae2ccb169ac92714dae8597fa95049ccf74fd651
MinGyu2 <mq04034@naver.com>
2023-01-16 18:43:57 +0900

    readme 추가

--- app/src/test/java/filetobyte/AppTest.java
+++ app/src/test/java/filetobyte/AppTest.java
@@ -3,9 +3,6 @@
  */
 package filetobyte;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
 class AppTest {
     // @Test void appHasAGreeting() {
     //     App classUnderTest = new App();
